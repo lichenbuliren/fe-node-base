4f10c008953c65fd3a095d3fa96d5b0eac7baf94
lichenbuliren <617106268@qq.com>
2021-04-25 19:48:06 +0800

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  extends: ['alloy', 'alloy/typescript'],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
